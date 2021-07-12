c3c9ed71ff20838993d303eb2b0a1f7d44b6a001
YeseniiaBorzova <yeseniiaborzova@gmail.com>
2021-07-12 21:06:09 +0300

    Corrected index.html, added login.html

--- film_project/services/web/project/__init__.py
+++ film_project/services/web/project/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_restx import Api
+
 from .main_page import main_blueprint
 from .models import db
 
 
 app = Flask(__name__)
+api = Api()
 app.register_blueprint(main_blueprint, url_prefix="")
-#app.config.from_object("film_project.services.web.project.config.Config")
-#app.config.from_pyfile('config.py')
+api.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hello_flask:hello_flask@db:5432/hello_flask_dev"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ENV'] = 'development'
 db.init_app(app)
-#engine = create_engine("postgresql://hello_flask:hello_flask@localhost:5433/hello_flask_dev")
-
 migrate = Migrate(app, db)
--- film_project/services/web/project/main_page.py
+++ film_project/services/web/project/main_page.py
@@ -4,14 +4,14 @@ from . import models
 main_blueprint = Blueprint('main_blueprint', __name__, template_folder='templates')
 
 
-@main_blueprint.route("/kek")
-def kek():
-    return jsonify(json_list=[i.serialize for i in models.Film.query.all()])
-
-
 @main_blueprint.route("/")
 def index():
     rows_per_page = 10
     page = request.args.get('page', 1, type=int)
     films = models.Film.query.paginate(page=page, per_page=rows_per_page)
     return render_template('index.html', films=films)
+
+
+@main_blueprint.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
