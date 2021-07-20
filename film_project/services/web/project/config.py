2fef4cba01b3c982a4748952c352985705f06bc7
YeseniiaBorzova <yeseniiaborzova@gmail.com>
2021-07-20 18:40:38 +0300

    Trying to implement prod docker

--- film_project/services/web/project/config.py
+++ film_project/services/web/project/config.py
@@ -9,6 +9,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     """Config app class"""
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL",
-                                        "postgresql://hello_flask:hello_flask@films:5432/hello_flask_dev")
+                                        "postgresql://hello_flask:hello_flask@localhost:5432/hello_flask_dev")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'development'
