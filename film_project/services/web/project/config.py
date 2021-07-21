5c6365fe1860ad933a1c13eff67a7087641bfa22
YeseniiaBorzova <yeseniiaborzova@gmail.com>
2021-07-21 11:45:59 +0300

    Added prod end and prod docker compose

--- film_project/services/web/project/config.py
+++ film_project/services/web/project/config.py
@@ -9,6 +9,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     """Config app class"""
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL",
-                                        "postgresql://hello_flask:hello_flask@localhost:5432/hello_flask_dev")
+                                        "postgresql://hello_flask:hello_flask@film_db:5432/hello_flask_prod")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASK_ENV = 'development'
